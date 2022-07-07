01e1f663750bfa13a8cee280e6bc25a1a2856b13
CodeCraft Plugin <86466022+CodeCraftPlugin@users.noreply.github.com>
2022-07-07 20:21:15 +0530

    added .gitignore

--- main.py
+++ main.py
@@ -1,30 +1,26 @@
 import numpy as np
 
-#convert CSV to NumPy array.]
-tp = np.genformtxt("")
+# convert CSV to NumPy array.]
+tp = np.genfromtxt("temperature_data.csv", delimiter=',')
+print(tp)
+
+# Slice the required array elements.
 
 
-#Slice the required array elements.
+# Increase temperatur values by 3.
 
 
-
-#Increase temperatur values by 3.
-
-
-#Slice monday temperature.
+# Slice monday temperature.
 
 
-#slice thursday and friday 6 am temperature values.
+# slice thursday and friday 6 am temperature values.
 
 
-#find the temperature values less than 20 or greater than 20
-
- 
-#DIY
-#find the average temperature of Tuesday
+# find the temperature values less than 20 or greater than 20
 
 
+# DIY
+# find the average temperature of Tuesday
 
-#Temperature conversion from Celsius to fahrenheit
 
-
+# Temperature conversion from Celsius to fahrenheit
