74f39c527c228c31116451f74f4dddef6160469f
CodeCraft Plugin <86466022+CodeCraftPlugin@users.noreply.github.com>
2022-07-07 14:42:08 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,29 @@
+import numpy as np
+
+#convert CSV to NumPy array.
+
+
+#Slice the required array elements.
+
+
+
+#Increase temperatur values by 3.
+
+
+#Slice monday temperature.
+
+
+#slice thursday and friday 6 am temperature values.
+
+
+#find the temperature values less than 20 or greater than 20
+
+ 
+#DIY
+#find the average temperature of Tuesday
+
+
+
+#Temperature conversion from Celsius to fahrenheit
+
+
